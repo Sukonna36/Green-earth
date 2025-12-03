315a37c3d6d532dc1055e04e7057babc9c4d9a01
HPelitebook840g3 <sukonna36@gmail.com>
2025-12-03 21:12:12 +0600

    Add dynamic category buttons and active state toggle

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const categoryContainer = document.getElementById('category-container');
+const allTreesButton = document.querySelector("#category-container button");
+
 
 const fetchPlantData = async () => {
   const request = await fetch(`https://openapi.programming-hero.com/api/plants`);
@@ -15,14 +17,38 @@ const fetchDatafromCategory = async () => {
 const init = async () => {
   const allCategories = await fetchDatafromCategory();
   const allPlantData = await fetchPlantData();
-  console.log(allCategories, allPlantData);
+
+  const allButton = document.createElement("button");
+  allButton.classList.add("btn", "btn-block", "shadow", "btn-category");
+  allButton.innerText = "All Trees";
+  categoryContainer.appendChild(allButton);
+
+
+  allButton.addEventListener("click", () => {
+  document.querySelectorAll(".btn-category").forEach(btn => {
+    btn.classList.remove("active-category");
+  });
+
+  allButton.classList.add("active-category");
+  console.log("clicked: All Trees");
+});
+
 
   allCategories.categories.map((category) => {
-    const buttonElement = `<button class="btn btn-block shadow btn-category">
-            ${category.category_name}
-          </button>`;
-    categoryContainer.innerHTML += buttonElement;
-  })
+    const button = document.createElement("button");
+    button.classList.add("btn", "btn-block", "shadow","btn-category");
+    button.innerText =category.category_name;
+
+    button.addEventListener("click", () =>{
+      document.querySelectorAll(".btn-category").forEach(btn =>{
+        btn.classList.remove("active-category");
+
+      });
+      button.classList.add("active-category");
+      console.log("clicked", category.category_name)
+    })
+    categoryContainer.appendChild(button);
+           })
 
 }
 
